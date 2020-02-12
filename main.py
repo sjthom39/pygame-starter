d395df046a9c377c4b3cd2477d90df34ae44a117
Stephen Thomas <sjthom39>
2020-02-12 10:10:02 -0500

    pictures for games

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import pygame # import library
-pygame.init()
 
+pygame.init()
+#spygame.image.load(samuri).convert()
 # Create the window
 win = pygame.display.set_mode((800, 600))
-
+img=pygame.image.load('assets/samuri.png')
 run = True
 while run:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
 
-pygame.image.load([your filename here]).convert()
-
-
 # Game code starts here ---------------------
   win.fill((0, 0, 0))
-
+  win.blit(img, (400,300))
   # Draw a rectangle
   pygame.draw.rect(win, (0, 204, 102), (50, 50, 100, 200))
   
